d69aae45c8c95543481b053db8bdd5a7dd5c311b
Tamás Varga <varga.tamas.pro@gmail.com>
2026-07-21 20:51:47 +0200

    feat: instantiate babyNames with dummy values and show first name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+
+  const [babyNames, setBabyNames] = useState([
+    {
+      id: 1,
+      name: 'Amelie'
+    }, {
+      id: 2,
+      name: 'Bethany'
+    }, {
+      id: 3,
+      name: 'Carol'
+    }, {
+      id: 4,
+      name: 'Denise'
+    }, {
+      id: 5,
+      name: 'Emily'
+    }
+  ]);
+
   return (
     <>
+      <h1>{babyNames[0].name}</h1>
     </>
   )
+
 }
 
 export default App;
