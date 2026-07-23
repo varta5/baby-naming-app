47158baf46d98bce4f311935deac5215a8359094
Tamás Varga <varga.tamas.pro@gmail.com>
2026-07-23 22:07:46 +0200

    feat: show Loading... while loading babyNames

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,21 @@ function App() {
   const [babyNames, setBabyNames] = useState([]);
 
   const [displayedBabyName, setDisplayedBabyName] = useState(null);
+  const [loadingBabyNames, setLoadingBabyNames] = useState(false);
 
   function loadRandomBabyName() {
     setDisplayedBabyName(babyNames[Math.floor(babyNames.length * Math.random())]);
   }
 
+  function LoadRandomBabyNameButton() {
+    return (<button onClick={loadRandomBabyName}>Get random baby name!</button>)
+  }
+
   async function loadBabyNames() {
+    setLoadingBabyNames(true);
     const response = await axios.get("http://localhost:8080/api/v1/names");
     setBabyNames(response.data);
+    setLoadingBabyNames(false);
   }
 
   useEffect(() => {
@@ -23,7 +30,7 @@ function App() {
 
   return (
     <>
-      <button onClick={loadRandomBabyName}>Get random baby name!</button>
+      {loadingBabyNames ? "Loading..." : <LoadRandomBabyNameButton />}
       <h1>{displayedBabyName?.name}</h1>
     </>
   )
