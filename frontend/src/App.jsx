2727134c64987c87f07ccd167d90e6c31c5bc852
Tamás Varga <varga.tamas.pro@gmail.com>
2026-08-01 08:24:37 +0200

    feat: move Voting to specific route using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,13 @@
-import { useEffect, useState } from 'react';
 import './styles/App.css';
-import VoteButtons from './components/VoteButtons';
-import { getBabyNames } from './services/api';
+import { Routes, Route } from 'react-router-dom';
+import Voting from './components/Voting';
 
 function App() {
 
-  const [babyNames, setBabyNames] = useState([]);
-  const [displayedBabyName, setDisplayedBabyName] = useState(null);
-
-  function loadNewRandomBabyName() {
-    removeBabyNameFromList();
-    loadRandomBabyName();
-  }
-
-  function removeBabyNameFromList() {
-    const index = babyNames.indexOf(displayedBabyName);
-    babyNames.splice(index, 1);
-  }
-
-  function loadRandomBabyName() {
-    setDisplayedBabyName(babyNames[Math.floor(babyNames.length * Math.random())]);
-  }
-
-  async function loadBabyNames() {
-    const response = await getBabyNames();
-    setBabyNames(response.data);
-  }
-
-  useEffect(() => {
-    loadBabyNames();
-  }, []);
-
-  useEffect(() => {
-    loadRandomBabyName();
-  }, [babyNames]);
-
   return (
-    <div class="app">
-      <h1>Would you like this name for your baby?</h1>
-      <h2>Number of names you could choose from: {babyNames.length}</h2>
-      <button onClick={loadBabyNames}>Refresh possible names</button>
-      <h2>{displayedBabyName?.name}</h2>
-      <VoteButtons babyNameId={displayedBabyName?.id} actionAfterVote={loadNewRandomBabyName}/>
-    </div>
+    <Routes>
+      <Route path="/voting" element={<Voting />} />
+    </Routes>
   )
 
 }
---
+++ frontend/src/components/Voting.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import VoteButtons from './VoteButtons';
+import { getBabyNames } from './../services/api';
+
+function Voting() {
+
+  const [babyNames, setBabyNames] = useState([]);
+  const [displayedBabyName, setDisplayedBabyName] = useState(null);
+
+  function loadNewRandomBabyName() {
+    removeBabyNameFromList();
+    loadRandomBabyName();
+  }
+
+  function removeBabyNameFromList() {
+    const index = babyNames.indexOf(displayedBabyName);
+    babyNames.splice(index, 1);
+  }
+
+  function loadRandomBabyName() {
+    setDisplayedBabyName(babyNames[Math.floor(babyNames.length * Math.random())]);
+  }
+
+  async function loadBabyNames() {
+    const response = await getBabyNames();
+    setBabyNames(response.data);
+  }
+
+  useEffect(() => {
+    loadBabyNames();
+  }, []);
+
+  useEffect(() => {
+    loadRandomBabyName();
+  }, [babyNames]);
+
+  return (
+    <div class="app">
+      <h1>Would you like this name for your baby?</h1>
+      <h2>Number of names you could choose from: {babyNames.length}</h2>
+      <button onClick={loadBabyNames}>Refresh possible names</button>
+      <h2>{displayedBabyName?.name}</h2>
+      <VoteButtons babyNameId={displayedBabyName?.id} actionAfterVote={loadNewRandomBabyName}/>
+    </div>
+  )
+
+}
+
+export default Voting;
